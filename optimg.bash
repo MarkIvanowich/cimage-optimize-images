d76c05a0a368cbba4c0bb418df39f73eb8d34b35
Mikael Roos <mikael.t.h.roos@gmail.com>
2015-06-23 14:05:06 +0200

    code formatting

--- optimg.bash
+++ optimg.bash
@@ -24,19 +24,18 @@ function optimg()
             output="PNG-image:  $img "
             optipng -o1 --quiet "$img"
             pngcrush -q "$img" /tmp/$$; mv /tmp/$$ "$img"
-        fi
-
-        if [ "${img##*.}" = "jpg" -o "${img##*.}" = "jpeg" ]; then
+        elif [ "${img##*.}" = "jpg" -o "${img##*.}" = "jpeg" ]; then
             output="JPEG-image: $img "
             jpegtran -copy none -optimize -outfile /tmp/$$ "$img"; mv /tmp/$$ "$img"
-        fi
+        else
+			continue;
+		fi
 
         output_file_size=$(stat -c%s "$img")
         diff=$(( diff + input_file_size - output_file_size ))
         percent=$( echo "scale=2; $output_file_size*100/$input_file_size" | bc )
 
         printf '%s %*.*s %6s\n' "$output" 0 $(( ${#pad} - ${#output} - 8 )) "$pad" "$percent"
-
     done
 
     echo "Saved " $(( diff / 1000 )) " kB."
